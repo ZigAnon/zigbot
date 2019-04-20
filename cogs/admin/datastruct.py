fd64c4f48077030c03b24a092697a295d0a1c9f7
ZigAnon <zb@zigbot.us.to>
2019-04-20 00:32:36 -0500

    Added WHERE col LIKE sql query to edit datastruct

--- cogs/admin/datastruct.py
+++ cogs/admin/datastruct.py
@@ -49,10 +49,49 @@ class DataStructCog(commands.Cog):
     if __name__ == '__main__':
         connect()
 
+    def update_vendor(role_id, role_perms):
+        """ update vendor name based on the vendor id """
+
+        sql = """ UPDATE roles
+                  SET role_perms = %s
+                  WHERE role_id = %s """
+        conn = None
+        updated_rows = 0
+        try:
+            # read database configuration
+            # params = config()
+            # connect to the PostgreSQL database
+            conn = dbSQL.connect(host = 'localhost',
+                                 database=_var.dbName,
+                                 user=_var.dbUser,
+                                 password=_var.dbPass)
+            # create a new cursor
+            cur = conn.cursor()
+            # execute the UPDATE  statement
+            cur.execute(sql, (role_perms, role_id))
+            # get the number of updated rows
+            updated_rows = cur.rowcount
+            # Commit the changes to the database
+            conn.commit()
+            # Close communication with the PostgreSQL database
+            cur.close()
+        except (Exception, dbSQL.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return updated_rows
+
     @commands.command(name='connect', hidden=True)
     @commands.is_owner()
     async def postgre_version(self, ctx):
         DataStructCog.connect()
+
+    @commands.command(name='connectt', hidden=True)
+    @commands.is_owner()
+    async def role_perms(self, ctx):
+        print(DataStructCog.update_vendor(526427381989376037,1))
 
 
 def setup(bot):
