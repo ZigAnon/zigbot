5a0c7689aba803e9b0cbe1879f28357e1ca1850b
ZigAnon <zb@zigbot.us.to>
2019-04-22 15:33:01 -0500

    initial heartbeat module

---
+++ bin/hb.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3.6
+import discord
+from discord.ext import commands
+from bin import zb_config
+
+_var = zb_config
+
+async def _heartbeat(bot):
+    print('working')
+
+    return
